579b7d3eef9827367bcac29394cb8db42e5bb76b
wjingyao2008 <yanyan@adobe.com>
2017-07-17 17:06:59 +0800

    remove duplicate configuration

--- src/main/java/yang/yang/springbootdemo/config/DependencyConfig.java
+++ src/main/java/yang/yang/springbootdemo/config/DependencyConfig.java
@@ -23,48 +23,48 @@ import java.util.Map;
 @ComponentScan(basePackages = { "yang.yang.springbootdemo"})
 public class DependencyConfig {
 
-    @Bean
-    @Autowired
-    @Primary
-    public PlatformTransactionManager transactionManager(EntityManagerFactory ptvEntityManagerFactory) {
-        JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(ptvEntityManagerFactory);
-        return txManager;
-    }
-
-    @Bean(name = "entityManagerFactory")
-    @Autowired
-    @Primary
-    public LocalContainerEntityManagerFactoryBean ptvEntityManagerFactory(DataSource statsDataSource) {
-
-        return createEntityManager(statsDataSource,
-                                   "yang.yang.springbootdemo.entity"
-        );
-    }
-
-    protected LocalContainerEntityManagerFactoryBean createEntityManager(DataSource dataSource,
-                                                                         String... packagesToScan) {
-        // hibernate adapter
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(false);
-        vendorAdapter.setDatabase(Database.MYSQL);
-
-        Map<String, String> props = new HashMap<>();
-        props.put("hibernate.temp.use_jdbc_metadata_defaults", "false");
-        props.put("hibernate.dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
-        //props.put("hibernate.cache.use_query_cache", "true");
-
-        // entity manager factory bean
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-
-        factory.setPersistenceUnitName("yangyang");
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPackagesToScan(packagesToScan);
-        factory.setDataSource(dataSource);
-        factory.setJpaPropertyMap(props);
-
-        return factory;
-    }
+//    @Bean
+//    @Autowired
+//    @Primary
+//    public PlatformTransactionManager transactionManager(EntityManagerFactory ptvEntityManagerFactory) {
+//        JpaTransactionManager txManager = new JpaTransactionManager();
+//        txManager.setEntityManagerFactory(ptvEntityManagerFactory);
+//        return txManager;
+//    }
+//
+//    @Bean(name = "entityManagerFactory")
+//    @Autowired
+//    @Primary
+//    public LocalContainerEntityManagerFactoryBean ptvEntityManagerFactory(DataSource statsDataSource) {
+//
+//        return createEntityManager(statsDataSource,
+//                                   "yang.yang.springbootdemo.entity"
+//        );
+//    }
+//
+//    protected LocalContainerEntityManagerFactoryBean createEntityManager(DataSource dataSource,
+//                                                                         String... packagesToScan) {
+//        // hibernate adapter
+//        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//        vendorAdapter.setGenerateDdl(false);
+//        vendorAdapter.setDatabase(Database.MYSQL);
+//
+//        Map<String, String> props = new HashMap<>();
+//        props.put("hibernate.temp.use_jdbc_metadata_defaults", "false");
+//        props.put("hibernate.dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
+//        //props.put("hibernate.cache.use_query_cache", "true");
+//
+//        // entity manager factory bean
+//        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+//
+//        factory.setPersistenceUnitName("yangyang");
+//        factory.setJpaVendorAdapter(vendorAdapter);
+//        factory.setPackagesToScan(packagesToScan);
+//        factory.setDataSource(dataSource);
+//        factory.setJpaPropertyMap(props);
+//
+//        return factory;
+//    }
 
 
 }
